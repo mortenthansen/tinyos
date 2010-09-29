61a8224fa06a9dc8dedbbeb013bf634ffeb9a0b2
Morten Tranberg Hansen <mth@cs.au.dk>
2010-09-29 15:25:47 +1000

    source script to correctly set TOS environment

---
+++ tinyos.sh
@@ -0,0 +1,7 @@
+# TinyOS Environment
+export TOSROOT=`pwd`
+export TOSDIR=$TOSROOT/tos
+export CLASSPATH=$TOSROOT/support/sdk/java/tinyos.jar:.
+export PYTHONPATH=$TOSROOT/support/sdk/python:.
+export MAKERULES=$TOSROOT/support/make/Makerules
+export PATH=/opt/msp430/bin:$PATH
